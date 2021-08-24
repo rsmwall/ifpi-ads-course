8ae95285139451da250530f1583e7eb06a0b3235
Rafael Silva <79131675+rsmwall@users.noreply.github.com>
2021-08-24 02:48:49 -0300

    Delete ads-2021.2/programacao-orientada-objetos/listas/lista-revisao-js directory

--- ads-2021.2/programacao-orientada-objetos/listas/lista-revisao-js/q01-antecessor-sucessor.js
+++
@@ -1,9 +0,0 @@
-const input = require('prompt-sync')()
-
-function main(){
-    const a = Number(input("Digite um número: "))
-
-    console.log(`O antecessor de ${a} é ${a-1} e o sucessor é ${a+1}`)
-}
-
-main()
