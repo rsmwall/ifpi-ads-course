aa4ad8ef95ed3d7ad0e20375e043d6ad253974e6
Rafael Silva <79131675+rsmwall@users.noreply.github.com>
2021-09-19 18:21:33 -0300

    Add files via upload

---
+++ ads-2021.2/estrutura-dados-l/listas/lista-03/q2-vetor-c-dobro.cpp
@@ -0,0 +1,47 @@
+/*
+Leia 2 vetores A e B com N elementos, escreva um vetor C, sendo este a junção das vetores
+A e B. Desta forma, a vetor C deverá ter 2*N elementos.
+*/
+
+#include <stdio.h>
+#define n 3
+
+int main(){
+    int a[n], b[n], c[2*n], i, j;
+
+    printf("\nVETOR A:\n");
+
+    for(i = 0; i < n; i++){
+        printf("Digite um número: ");
+        scanf("%d", &a[i]);
+        c[i] = a[i];
+    }
+
+    printf("\nVETOR B:\n");
+
+    for(int l = 0; l < n; l++){
+        j = i;
+        printf("Digite um número: ");
+        scanf("%d", &b[l]);
+        c[j] = b[l];
+        i++;
+    }
+
+    printf("\nA: ");
+
+    for(int l = 0; l < n; l++){
+        printf("%d ", a[l]);
+    }
+
+    printf("\nB: ");
+
+    for(int l = 0; l < n; l++){
+        printf("%d ", b[l]);
+    }
+
+    printf("\nc: ");
+
+    for(int l = 0; l < 2*n; l++){
+        printf("%d ", c[l]);
+    }
+}
