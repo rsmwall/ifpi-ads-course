3463a0216099e85d3385223dd3adc9bd1a9e09a0
Rafael <rafaelrsilva.dev@gmail.com>
2023-08-16 16:38:00 -0300

    Introducao C/C++

---
+++ ads-2023.2/estrutura-dados-i/conteudo/introducao/iteracao/while.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int contador;
+    contador = 1;
+
+    while (contador <= 10) {
+        cout << contador << endl;
+        contador++;
+    }
+    return 0;
+}
