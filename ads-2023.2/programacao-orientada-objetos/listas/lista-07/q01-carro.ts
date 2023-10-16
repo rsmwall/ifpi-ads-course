df8ca886153ebb52fcc579eced4d85b29c5f4462
Rafael <rafaelrsilva.dev@gmail.com>
2023-10-16 15:32:36 -0300

    Update q01-carro.ts

--- ads-2023.2/programacao-orientada-objetos/listas/lista-07/q01-carro.ts
+++ ads-2023.2/programacao-orientada-objetos/listas/lista-07/q01-carro.ts
@@ -7,12 +7,27 @@
 class Veiculo {
     placa: string
     ano: number
+
+    constructor(placa: string, ano: number) {
+        this.placa = placa
+        this.ano = ano
+    }
 }
 
 class Carro extends Veiculo {
     modelo: string
+
+    constructor(placa: string, ano: number, modelo: string) {
+        super(placa, ano)
+        this.modelo = modelo
+    }
 }
 
 class CarroEletrico extends Carro {
     autonomiaBateria: number
+
+    constructor(placa: string, ano: number, modelo: string, autonomiaBateria: number) {
+        super(placa, ano, modelo)
+        this.autonomiaBateria = autonomiaBateria
+    }
 }
